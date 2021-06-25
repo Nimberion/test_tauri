c2d90a6074861fca3cf32cb1d104f374b8ffb725
Nimberion <philipp.volkmer@web.de>
2021-06-25 17:24:00 +0200

    added writeFile and readTextFile APIs

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,26 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
+    <div>{{ textFile }}</div>
     <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
 import HelloWorld from "@/components/HelloWorld.vue";
+import { writeFile, readTextFile } from "@tauri-apps/api/fs";
 
-export default {
+@Component({
   name: "Home",
-  components: {
-    HelloWorld,
-  },
-};
+  components: { HelloWorld },
+})
+export default class Home extends Vue {
+  textFile = "nope";
+
+  async created(): Promise<void> {
+    writeFile({ contents: "{Hallo}", path: "test_tauri.json" });
+    this.textFile = await readTextFile("test_tauri.json");
+  }
+}
 </script>
